c243b861d5474f28e91b39607e6d72a9adea8e62
jassan leyva <jassan851031@gmail.com>
2020-01-08 12:39:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+  };
+
+  event(e) {
+    if (e.keyCode === 13 && e.target.value !== '') {
+      this.setState({
+        list: this.state.list.concat(e.target.value)
+      });
+      e.target.value = "";
+    }
+  }
+  trash(y) {
+    console.log(y);
+    const { list } = this.state;
+    list.splice(y, 1);
+    this.setState({
+      list: list
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className= "text-center" >TO DO LIST</h1>
+            <input type="text" className="form-control" placeholder="Add to do here" onKeyDown={e => this.event(e)} />
+            <br />
+            <ul className="list-group">
+              {
+                this.state.list.length > 0 &&
+                this.state.list.map((list, i) => {
+                  return (
+                    <li key={i} className="list-group-item d-flex justify-content-between">
+                      {list}
+                      <i className="fa fa-trash" onClick={y => this.trash(i)}></i>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
+
+
+    import 'jquery';
+    import 'popper.js';
+    import 'bootstrap';
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
